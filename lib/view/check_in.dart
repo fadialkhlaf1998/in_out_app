7757d17f62bf95a7c0378c5e89a962d0ad013be7
fadialkhlaf1998 <fadi.kh38610130@gmail.com>
2022-09-28 15:35:43 +0300

    photo

--- lib/view/check_in.dart
+++ lib/view/check_in.dart
@@ -25,81 +25,82 @@ class CheckIn extends StatelessWidget {
       DeviceOrientation.portraitDown,
     ]);
     return Scaffold(
-
       extendBodyBehindAppBar: true,
-      backgroundColor: App.navyBlue,
+      backgroundColor: App.smallBtnBG,
       body: Obx((){
-        return Container(
-            width: MediaQuery.of(context).size.width,
-            child: Center(
-              child: Stack(
-                children: [
-                  checkInController.fake.value?Center():Center(),
-                  Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/new_icons/background.jpg")
-                      )
-                    ),
-                    child: Container(
-                      width: Get.width,
-                      height: Get.height,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage("assets/new_icons/background.jpg"),
-                              fit: BoxFit.cover
-                          )
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: Get.width*0.9,
-                            height: 120,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-
-                                _logout(Colors.transparent),
-                                Container(
-                                  width: MediaQuery.of(context).size.width*0.5,
-                                  height: 120,
-                                  child: Center(
-                                      child: SvgPicture.network(Global.employee!.company_image),
-                                  ),
-                                ),
-                                _logout(Colors.white),
-
-                              ],
-                            ),
-                          ),
-                          Container(
-                            width: Get.width,
-                            height: Get.height-120-MediaQuery.of(context).padding.top-Get.bottomBarHeight,
-                            child: statesView(context),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  checkInController.bottomSheetOpened.value?Center():Positioned(child:  bottomSheetBtn(context),bottom: 0,),
-                  AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 300),
-                    child: checkInController.loading.value
-                        ?
+        return SafeArea(
+          child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: Stack(
+                  children: [
+                    checkInController.fake.value?Center():Center(),
                     Container(
-                       // color: Colors.white.withOpacity(0.3),
                       decoration: BoxDecoration(
-                        color: App.navyBlue,
                         image: DecorationImage(
-                          image: AssetImage('assets/images/background.png')
+                          image: AssetImage("assets/new_icons/background.jpg")
                         )
                       ),
-                        child: App.Loading()) : Text(''),
-                  )
-                ],
-              ),
-            )
+                      child: Container(
+                        width: Get.width,
+                        height: Get.height,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("assets/new_icons/background.jpg"),
+                                fit: BoxFit.cover
+                            )
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: Get.width*0.9,
+                              height: 120,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+
+                                  _logout(Colors.transparent),
+                                  Container(
+                                    width: MediaQuery.of(context).size.width*0.5,
+                                    height: 120,
+                                    child: Center(
+                                        child: SvgPicture.network(Global.employee!.company_image),
+                                    ),
+                                  ),
+                                  _logout(Colors.white),
+
+                                ],
+                              ),
+                            ),
+                            Container(
+                              width: Get.width,
+                              height: Get.height-120-MediaQuery.of(context).padding.top-Get.bottomBarHeight,
+                              child: statesView(context),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    checkInController.bottomSheetOpened.value?Center():Positioned(child:  bottomSheetBtn(context),bottom: 0,),
+                    AnimatedSwitcher(
+                      duration: const Duration(milliseconds: 300),
+                      child: checkInController.loading.value
+                          ?
+                      Container(
+                         // color: Colors.white.withOpacity(0.3),
+                        decoration: BoxDecoration(
+                          color: App.navyBlue,
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/background.png')
+                          )
+                        ),
+                          child: App.Loading()) : Text(''),
+                    )
+                  ],
+                ),
+              )
+          ),
         );
       }),
     );
@@ -113,7 +114,7 @@ class CheckIn extends StatelessWidget {
         bottomSheet(context);
       },
       child: Container(
-        height: 100,
+        height: 50,
         width: Get.width,
         // color: Colors.red,
         child: Stack(
