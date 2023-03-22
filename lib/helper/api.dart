350b204b3055ffe6153ed9933034fad13a349c58
Fado Alkhlaf Omen <fadi.kh38610130@gmail.com>
2023-03-22 12:38:48 +0200

    photo

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -13,8 +13,9 @@ import 'package:in_out_app/model/workHours.dart';
 class Api {
   // static String url = "https://phpstack-548447-2849982.cloudwaysapps.com/";
   // static String url = "http://ec2-35-77-99-214.ap-northeast-1.compute.amazonaws.com:3000/";
-  static String url = "http://35.73.198.186:3000/";
-  // static String url = "http://10.0.2.2:3000";
+  // static String url = "http://35.73.198.186:3000/";
+  static String url = "http://in-out-1275230880.ap-northeast-1.elb.amazonaws.com/";
+
   static String token="";
 
   static Future<Employee?> login(String username,String password)async{
